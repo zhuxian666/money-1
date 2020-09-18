d4ae06ebf46d70cd898eca9809f639b5d5f1d4fd
杨程 <7702269+yangcheng666@user.noreply.gitee.com>
2020-09-18 10:23:29 +0800

    更新组件实现跳转 做404

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,5 +1,11 @@
 <template>
-   <div> 404</div>
+   <div class="not-found">
+      <h1>404</h1>
+      <div>当前页面不存在，请检查网址是否正确</div>
+      <div>
+         <router-link to="/">返回首页</router-link>
+      </div>
+   </div>
 </template>
 
 <script lang=ts>
@@ -12,6 +18,8 @@
   }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.not-found{
 
+}
 </style>
